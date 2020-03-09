fd818d4bcfc7fe081a7ced09832c6d60e2e91015
Diego Alejandro <martineztellezd@gmail.com>
2020-03-09 14:42:05 -0600

    Add files via upload

---
+++ T01.py
@@ -0,0 +1,20 @@
+#Program T01_1941559
+#Diego Alejandro Tellez Martinez (1941559)
+#!Funcion de Programa: Leer tres numeros sumarlos, multiplicarlos y escribir el resultado
+
+n1 = input("Dame el primer numero")
+n2 = input("Dame el segundo numero")
+n3 = input("Dame el tercer numero")
+
+sum = n1 + n2 + n3 
+
+mul = n1*n2*n3 
+
+
+print("La suma de los numeros es" ,sum) 
+print("El producto de los numeros es",mul)
+
+
+
+
+
